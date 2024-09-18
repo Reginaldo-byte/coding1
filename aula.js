55ceb63bbceb5d80337778328a5149385b6dddd7
Reginaldo-byte <REGINALDO.SJUNIOR@edu.pe.senac.br>
2024-09-18 18:00:16 +0000

    criamos algoritimo boletim

---
+++ aula.js
@@ -0,0 +1,59 @@
+//Este código foi para treino de variáveis
+
+/*const rotulo = "Natalya";
+const rotulo = 12345;
+console.log(rotulo);
+*/
+//
+/*const numero1 = 10;
+const numero2 = 50;
+
+const soma = numero1 + numero2;
+const divisao = numero1 / numero2;
+const multiplicacao = numero1 * numero2;
+const subitracao = numero1 - numero2;
+
+console.log('O resultado da soma é' + ' '+soma);
+console.log('O resultado da divisão é' + ' '+divisao);
+console.log('O resultado da multiplicação é' + ' '+multiplicacao);
+console.log('O resultado da subitração é' + ' '+subitracao);
+
+const idade = 20
+const nome = 'Reginaldo'
+const cidade = 'paulista'
+console.log('olá,meu nome é ' + [nome] + ' e tenho ' + [idade] + ' de idade e moro em ' + [cidade]);
+
+const base = 30
+const altura = 20
+const area = base * altura / 2
+console.log ('A área do triangulo é ' + area)
+ 
+const nota1 = 9;
+const nota2 = 6;
+const nota3 = 3;
+const media = nota1 + nota2 + nota3 /3;
+
+console.log('A média de Reginaldo é ' + media);
+
+const preco_original = 100
+const desconto = 5
+const preco_final = preco_original * (1 - desconto / 100)
+
+console.log('preço final = ' + preco_final)
+
+const celsius = 29
+const fahrenheit = (celsius * 9/5) + 32
+
+console.log('O valor da converção é ' + fahrenheit)
+
+const salario_base = 1.832;
+const aliquota = 0.15;
+const valor_fiscal = salario_base - (salario_base * aliquota)
+
+console.log ('O valor final do ímposto de renda é ' + salario_base.toFixed(3))
+
+
+reais = 500
+dolar = 5.45
+conversao = reais / dolar 
+console.log('A conversão é ' + conversao.toFixed(2))*/
